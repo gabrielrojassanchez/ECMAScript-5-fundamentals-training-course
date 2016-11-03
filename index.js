d596770d66746376f8982c4d256a8c205f697f09
Ricardo Pina Arellano <ricardo.pina@adesis.com>
2016-11-03 00:13:40 -0600

    feat(index): review hoisting concept

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* Normal declared
 var car;
 
 function getInformation() {
@@ -6,3 +7,19 @@ function getInformation() {
 
 console.log(car);
 console.log(getInformation());
+*/
+
+/*
+* In JavaScript 5 all variables and functions declared are hoisted.
+* The hoisting means that all variables and functions declared will be moved to the top,
+* if this variables have values, these will be omited in declaration.
+*/
+
+console.log(car);
+console.log(getInformation());
+
+var car;
+
+function getInformation() {
+
+}
